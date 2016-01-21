cc97dafaf774bc3d8aa913835f0c71f915e25e79
Denis Maltsev <deni@rionix.com>
2016-01-21 22:08:09 +0300

    first commit

---
+++ fb2mp3.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+# Denis Maltsev
+# 21.01.2016
+
+import sys, os, codecs
+import xml.parsers.expat
+import pyvona
+
+book = []
+par = ''
+body = False
+
+#######################
+
+def start_element(name, attrs):
+	global body, par
+	if name == 'body':
+		body = True
+	if name == 'p':
+		par = '';
+
+def end_element(name):
+	global body, par, book
+	if name == 'body':
+		body = False
+	if body == False:
+		return
+	if name == 'p':
+		par = par.strip()
+		if len(par) > 0:
+			book.append(par)
+	if name not in ['strong', 'emphasis', 'style']:
+		par = ''
+
+def char_data(data):
+	global par
+	par = par + data
+
+def	parse_fb2(book_name):
+	book_file = codecs.open(book_name, 'r', 'utf-8')
+	p = xml.parsers.expat.ParserCreate()
+	p.StartElementHandler = start_element
+	p.EndElementHandler = end_element
+	p.CharacterDataHandler = char_data
+	p.Parse(book_file.read(), True)
+
+#######################
+
+def make_mp3(book_name):
+	global book
+
+	voice = pyvona.create_voice('GDNAICTDMLSLU5426OAA', '2qUFTF8ZF9wqy7xoGBY+YXLEu+M2Qqalf/pSrd9m')
+	voice.codec = 'mp3'
+	voice.region = 'ru-RU'
+	voice.speech_rate = 'fast' # x-slow, slow, medium, fast, x-fast
+	voice.voice_name = 'Maxim' # Tatyana, Maxim 
+
+	title = os.path.splitext(book_name)[0]
+	if not os.path.exists(title):
+		os.makedirs(title)
+
+	index = 0
+	for par in book:
+		if len(par) > 3:
+			file_name = '{0}/{0}{1:05}.mp3'.format(title, index) 
+			voice.fetch_voice(par, file_name)
+			index += 1
+
+#######################
+
+def main(book_name):
+	parse_fb2(book_name)
+	make_mp3(book_name)
+
+if __name__ == '__main__':
+	if len(sys.argv) > 1:
+		main(sys.argv[1])
